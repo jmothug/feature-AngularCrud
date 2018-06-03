8e6520406a5e57c2581e958af6458073f1d420c4
joselio <jmothug@gmail.com>
2018-06-03 12:18:36 -0300

    cricao da pasta Models e do model Employee

---
+++ src/app/models/employee.model.ts
@@ -0,0 +1,11 @@
+export class Employee{
+    id: number;
+    name: string;
+    gender: string;
+    phoneNumber?: number;
+    contactPreference: string;
+    dateOfBirth: Date;
+    department: string;
+    isActive: boolean;
+    photoPath?: string;
+}
